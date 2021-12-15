6822715b17ef26e08b0c66f0f92b340b8bec671c
KeytFrey <katja-255870@mail.ru>
2021-12-15 14:03:50 +0300

    todo-list_js

--- app.js
+++ app.js
@@ -2,31 +2,75 @@ let inp = document.querySelector('.inp')
 let btn = document.querySelector('.btn')
 let tasks = document.querySelector('.tasks')
 
-
 let taskArr = []
 
 btn.addEventListener('click', () => {
-    taskArr.push(inp.value)
+    taskArr.push({checkbox: false, name: inp.value, flaginp: false})
     tasks.innerHTML = ''
     inp.value = ''
     renderTasks()
 })
 
-
-
 const renderTasks = () => {
     taskArr.forEach((elem) => {
+        // const inpCheckbox = document.createElement('input')
         const p = document.createElement('p')
+        const inpEdit = document.createElement('input')
         const btnDel = document.createElement('button')
-        btnDel.innerHTML = 'удалить'
-        p.innerHTML = elem
-        tasks.append(p)
-        p.append(btnDel)
+        const btnEdit = document.createElement('button')
+        const div = document.createElement('div')
+        // inpCheckbox.type = 'checkbox'
+        // inpCheckbox.classList.add('checkbox')
+        btnEdit.classList.add('btn-edit')
+        btnDel.classList.add('btn-del')
+        inpEdit.classList.add('inp-edit')
+        div.classList.add('task')
+        // inpCheckbox.addEventListener ('click', () => changeCheckbox(elem))
+        btnEdit.addEventListener ('click', () => EditTasks(elem))
+        btnDel.addEventListener ('click', () => deletTasks(elem))
 
-        btnDel.addEventListener ('click', () => {
-            p.remove()
-            taskArr.splice(p, 1)
-            console.log(taskArr)
-        })
+        // if (elem.checkbox) {
+        //     p.style.textDecoration = 'line-through'
+        //     div.append(p)
+        // }else {
+        //     p.style.textDecoration = 'none'
+        //     div.append(p)
+        // }
+
+        if(elem.flaginp) {
+            inpEdit.value = elem.name
+            inpEdit.addEventListener('keyup', (event) => changeInp(event.target.value, elem))
+            div.append(inpEdit)
+        }else {
+            p.innerHTML = elem.name
+            // div.append(inpCheckbox)
+            div.append(p)
+        }
+
+        div.append(btnEdit)
+        div.append(btnDel)
+        tasks.append(div)
     })
 }
+
+// function changeCheckbox(elem) {
+//     taskArr[taskArr.indexOf(elem)].checkbox = !taskArr[taskArr.indexOf(elem)].checkbox
+//     console.log(taskArr)
+// }
+
+function changeInp(value, elem) {
+    taskArr[taskArr.indexOf(elem)].name = value
+}
+
+function EditTasks(elem) {
+    taskArr[taskArr.indexOf(elem)].flaginp = !taskArr[taskArr.indexOf(elem)].flaginp
+    tasks.innerHTML = ''
+    renderTasks()
+}
+
+function deletTasks(elem) {
+    taskArr.splice(taskArr.indexOf(elem), 1)
+    tasks.innerHTML = ''
+    renderTasks()
+}
+
